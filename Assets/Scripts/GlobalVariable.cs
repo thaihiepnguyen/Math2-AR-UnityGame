f63fd93116eeabdb40093760194f1f28c470726e
nguyen tran dai duong <87960153+aduongdnai@users.noreply.github.com>
2024-04-19 12:01:36 +0700

    Update GlobalVariable.cs server url to localhost

--- Assets/Scripts/GlobalVariable.cs
+++ Assets/Scripts/GlobalVariable.cs
@@ -7,7 +7,7 @@ public class GlobalVariable
     public const int MAIN_SCENE = 1;
     public const int LOGIN_SCENE = 0;
     public const int REGISTER_SCENE = 2;
-    public const string server_url = "http://192.168.100.5:3000";
+    public const string server_url = "http://127.0.0.1:3000";
     public const string DragDropType = "DragDrop";
     public const string InputType = "Input";
     public const string MultipleChoiceType = "ABCD";
