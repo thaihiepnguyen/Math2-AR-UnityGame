128485ae37b246b0359ee84c9b836e1694eae778
hiepnt <thaihiep232002@gmail.com>
2024-04-23 15:10:19 +0700

    fix: refactor code

--- Assets/Scripts/BookView/BookViewManager.cs
+++ Assets/Scripts/BookView/BookViewManager.cs
@@ -6,21 +6,32 @@ using UnityEngine.UI;
 public class Rotate : MonoBehaviour
 {
     public Button backBtn;
+    public Button rotateBtn;
+    private bool _isVertical = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        Screen.orientation = ScreenOrientation.Portrait;
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
         backBtn.onClick.AddListener(OnClickBackBtn);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        rotateBtn.onClick.AddListener(() => OnClickRotateBtn(!this._isVertical));
     }
 
     public void OnClickBackBtn()
     {
         Screen.orientation = ScreenOrientation.LandscapeLeft;
+        this._isVertical = false;
+    }
+
+    public void OnClickRotateBtn(bool isVertical)
+    {
+        if (isVertical)
+        {
+            Screen.orientation = ScreenOrientation.Portrait;
+        } else
+        {
+            Screen.orientation = ScreenOrientation.LandscapeLeft;
+        }
+        this._isVertical = isVertical;
     }
 }
