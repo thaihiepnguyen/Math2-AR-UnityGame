2b2a28410b6a46453bb3a05bd5f4ce482c927487
dbchau10 <82007790+dbchau10@users.noreply.github.com>
2024-05-22 00:16:56 +0700

    feat: skin customization

--- Assets/Scripts/GridAlignment.cs
+++ Assets/Scripts/GridAlignment.cs
@@ -6,21 +6,24 @@ using UnityEngine.UI;
 public class GridAlignment : MonoBehaviour
 {
     public GameObject container;
-    void Update()
+    public int col = 3;
+    public  int row = 2;
+    void Start()
     
     {   
         var paddingHorizontal = (int) container.GetComponent<RectTransform>().rect.width * 1/20;
-        container.GetComponent<GridLayoutGroup>().padding.left = paddingHorizontal;
-        container.GetComponent<GridLayoutGroup>().padding.right = paddingHorizontal;
+        GetComponent<GridLayoutGroup>().padding.left = paddingHorizontal;
+        GetComponent<GridLayoutGroup>().padding.right = paddingHorizontal;
         var paddingVertical = (int) container.GetComponent<RectTransform>().rect.height * 1/20;
 
-        container.GetComponent<GridLayoutGroup>().padding.top = paddingVertical;
-        container.GetComponent<GridLayoutGroup>().padding.bottom = paddingVertical;
+        GetComponent<GridLayoutGroup>().padding.top = paddingVertical;
+        GetComponent<GridLayoutGroup>().padding.bottom = paddingVertical;
         float width = container.GetComponent<RectTransform>().rect.width - 2*paddingHorizontal;
         float height = container.GetComponent<RectTransform>().rect.height  - 2*paddingVertical; 
-        Vector2 newSize = new Vector2(width/3,height/2);
+        Debug.Log(height);
+        Vector2 newSize = new Vector2(width/col,height/row);
 
-        container.GetComponent<GridLayoutGroup>().cellSize = newSize;
+        GetComponent<GridLayoutGroup>().cellSize = newSize;
     }
 
 
