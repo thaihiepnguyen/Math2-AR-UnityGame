94639774fb576733ba915b6c164930b84ceef6e2
nguyen tran dai duong <87960153+aduongdnai@users.noreply.github.com>
2024-05-29 15:53:08 +0700

    feat: reinstall UnityYoubePlayer package and navigate video learning scene from lesson scene

--- Assets/Scripts/Lesson/Lesson.cs
+++ Assets/Scripts/Lesson/Lesson.cs
@@ -5,9 +5,29 @@ using UnityEngine;
 
 public class Lesson : MonoBehaviour
 {
-    void Start()
+    LessonBUS lessonBUS = new LessonBUS();
+    [SerializeField] TextMeshProUGUI title;
+    private static string VideoUrl;
+    public static string GetVideoUrl()
+    {
+        return VideoUrl;
+    }
+    LessonDTO lesson;
+    // Start is called before the first frame update
+    async void Start()
     {
 
+        int lessonId = int.Parse(LessonList.GetLessonId());
+
+        var lessonResponse = await lessonBUS.GetVideoByLessonId(lessonId);
+
+        if (lessonResponse.data != null)
+        {
+            lesson=lessonResponse.data;
+            VideoUrl = lesson.video_url;
+            title.text = lesson.name;
+
+        }
     }
 
     // Update is called once per frame
@@ -19,5 +39,10 @@ public class Lesson : MonoBehaviour
     public void OnClickDoingExercise()
     {
         SceneHistory.GetInstance().LoadScene(GlobalVariable.EXERCISES_SCENE);
+    }
+    public void OnClickViewLesson()
+    {
+        //if video url
+        SceneHistory.GetInstance().LoadScene("VideoLearning");
     }
 }
