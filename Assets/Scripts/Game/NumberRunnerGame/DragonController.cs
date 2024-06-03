92f3e8713e397454f5cc5f214fa5874772a240ad
dbchau10 <82007790+dbchau10@users.noreply.github.com>
2024-06-04 01:17:57 +0700

    feat: live meshing + navigation mesh

---
+++ Assets/Scripts/Game/NumberRunnerGame/DragonController.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonController : MonoBehaviour
+{
+    Animator dragonAnimation;
+    public float speedThreshold = 0.1f; // The speed threshold to change animation
+
+
+    private FixedJoystick joystick;
+
+    private Rigidbody rigidBody;
+    private Vector3 lastPosition;  // Store the last position
+    private float lastTime;        // Store the last time position was recorded
+
+    private WaitForSeconds quartSec = new WaitForSeconds(.25f);
+
+    
+    //Control the animation state, so that 
+    private DragonAnimationState AnimationState;
+    void Start()
+    {
+        dragonAnimation = GetComponent<Animator>();
+        joystick = FindObjectOfType<FixedJoystick>();
+        rigidBody = gameObject.GetComponent<Rigidbody>();
+
+        lastPosition = transform.position;  // Initialize last position
+        lastTime = Time.time;
+        StartCoroutine(ControlState());
+        AnimationState = DragonAnimationState.isIdle;
+    }
+
+    
+    void FixedUpdate()
+    {
+        float xVal = joystick.Horizontal;
+        float yVal = joystick.Vertical;
+
+        Vector3 movement = new Vector3(xVal,0,yVal);
+        rigidBody.velocity = movement*speedThreshold;
+
+        if (xVal !=0 && yVal!=0){
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x,Mathf.Atan2(xVal,yVal) *Mathf.Rad2Deg,transform.eulerAngles.z);
+        }
+    }
+    
+    public enum DragonAnimationState
+    {
+        isIdle, 
+        isRunning,
+    }
+    
+    IEnumerator ControlState()
+    {
+        while (true)
+        {
+            // Calculate elapsed time
+            float elapsedTime = Time.time - lastTime;
+
+            // Calculate the distance traveled since the last frame
+            float distance = Vector3.Distance(transform.position, lastPosition);
+
+            // Calculate the speed
+            float speed = distance / elapsedTime;
+
+            // Update last time and last position for the next frame
+            lastTime = Time.time;
+            lastPosition = transform.position;
+            
+            Debug.Log("CurrentState is => " + AnimationState);
+
+            
+            //Change Animation based on speed
+            if (speed > speedThreshold)
+            {
+                if (AnimationState != DragonAnimationState.isRunning)
+                {
+                    Debug.Log("Set to is Running, Animation State => " + AnimationState);
+                    ChangeToState("isRunning");
+                    AnimationState = DragonAnimationState.isRunning;
+                }
+            }
+            else if(AnimationState != DragonAnimationState.isIdle)
+            {
+                Debug.Log("Set to is Idle =>" + AnimationState);
+                ChangeToState("isIdle");
+                AnimationState = DragonAnimationState.isIdle;
+
+            }
+            
+            yield return quartSec;
+        }
+        yield return null;
+    }
+
+    //Change one bool to true and the others to false
+    void ChangeToState(string setToState)
+    {
+        foreach (var param in dragonAnimation.parameters)
+        {
+            if (param.type == AnimatorControllerParameterType.Bool)
+            {
+                dragonAnimation.SetBool(param.name, param.name == setToState);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Game/NumberRunnerGame/NavMeshHowTo.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using Niantic.Lightship.AR.NavigationMesh;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using System;
+
+
+public class NavMeshHowTo : MonoBehaviour
+{
+    [SerializeField]
+    private Camera _camera;
+
+
+    [SerializeField]
+    private LightshipNavMeshManager _navmeshManager;
+
+    [SerializeField]
+    private LightshipNavMeshAgent _agentPrefab;
+
+    
+    private LightshipNavMeshAgent _agentInstance;
+
+    void Update()
+    {
+        // if (isPlaced) return;
+        HandleTouch();
+    }
+
+    
+
+    public void SetVisualization(bool isVisualizationOn)
+    {
+        //turn off the rendering for the navmesh
+        _navmeshManager.GetComponent<LightshipNavMeshRenderer>().enabled = isVisualizationOn;
+
+        if (_agentInstance != null)
+        {
+            //turn off the path rendering on the active agent
+            _agentInstance.GetComponent<LightshipNavMeshAgentPathRenderer>().enabled = isVisualizationOn;
+        }
+    }
+
+
+
+     private void HandleTouch()
+    {
+        //in the editor we want to use mouse clicks, on phones we want touches.
+    #if UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2))
+    
+        
+    #else
+        var touch = Input.GetTouch(0);
+
+        //if there is no touch or touch selects UI element
+        if (Input.touchCount <= 0)
+            return;
+        if (touch.phase == UnityEngine.TouchPhase.Began)
+    #endif
+        {
+        #if UNITY_EDITOR
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+        #else
+            Ray ray = _camera.ScreenPointToRay(touch.position);
+        #endif
+            //project the touch point from screen space into 3d and pass that to your agent as a destination
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (_agentInstance == null )
+                {
+                    _agentInstance = Instantiate(_agentPrefab);
+                    _agentInstance.transform.position = hit.point;
+                }
+                else
+                {
+                    _agentInstance.SetDestination(hit.point);
+                }
+            }
+        }
+    }
+}
